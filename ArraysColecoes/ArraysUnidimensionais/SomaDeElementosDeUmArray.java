14bc77b211439d5e8fe73b6f11d6ccadb7aa5f5c
OkauanDev <teko4278@gmail.com>
2026-09-10 17:50:44 -0300

    finalizando o exercício 66 e parando na página 58

--- ArraysColecoes/ArraysUnidimensionais/SomaDeElementosDeUmArray.java
+++ ArraysColecoes/ArraysUnidimensionais/SomaDeElementosDeUmArray.java
@@ -1,4 +1,5 @@
 package ArraysColecoes.ArraysUnidimensionais;
+import java.util.Scanner;
 
 /*
     EXERCÍCIO 66
@@ -9,6 +10,20 @@ em um array e calcule a soma de todos os elementos.
 
 public class SomaDeElementosDeUmArray {
     public static void main(String[] args) {
+        Scanner scanner = new  Scanner(System.in);
         
+        int[] numeros = new int[5];
+        int soma = 0;
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println("Digite um número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+            soma += numeros[i];
+        }
+        
+        System.out.println("A soma dos elementos do array é: " + soma);
+        
+        
+        scanner.close();
     }
 }
